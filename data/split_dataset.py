bd438659aeb06cab418279baded90b8d91af1eb0
FlauFly <tirhhe@gmail.com>
2026-03-09 13:44:50 +0100

    first sketch of splitting script

--- data/split_dataset.py
+++ data/split_dataset.py
@@ -1,5 +1,16 @@
 import json
+import pandas as pd
 
 with open('data.json', 'r') as file:
     data = json.load(file)
 
+nodes = data["nodes"]
+links = data["links"]
+
+article = 'Plato'
+
+df_nodes = pd.DataFrame.from_dict(nodes)
+df_links = pd.DataFrame.from_dict(links)
+
+selected = df_links.loc[df_links['source'] == article]
+print(selected)
